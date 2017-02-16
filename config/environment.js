2a7f2431d708df001335449c3e705b41ed3c7f6a
AmberRivera <amberolive@gmail.com>
2017-02-16 10:22:58 -0500

    Refactor project as fieldwork-dispatcher. Add Firebase and Torii config.

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'kubtest',
+    modulePrefix: 'fieldwork-dispatcher',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
