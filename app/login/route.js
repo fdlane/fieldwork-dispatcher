b5ce775ff323f4595f30d60fd447b61407d96aff
jkc07100 <keith.clinard@kub.org>
2017-03-13 15:29:01 -0400

    feat: Added currentUser to routes,controllers, etc

--- app/login/route.js
+++ app/login/route.js
@@ -2,17 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  redirect() {
+    if (this.get('session.isAuthenticated')) {
+      this.transitionTo('protected');
+    }
+  },
+
   actions: {
 
     login(email, password) {
-
-      this.get('session').open('firebase', {
+      let session = this.get('session');
+      session.open('firebase', {
         provider: 'password',
         email,
         password
 
       }).then(() => {
 
+        let user = session.content.currentUser;
+        let email = user.email;
+        let username = email.substring(0, email.indexOf('@'));
+        user.username = username;
+        this.set('currentUser.content', user);
         this.transitionTo('protected');
 
       });
