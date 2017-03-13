0a24afe05ae7239432c846b484a64695687a22d0
AmberRivera <amberolive@gmail.com>
2017-03-13 15:02:03 -0400

    Add attributes to job and worker models.

--- app/worker/model.js
+++ app/worker/model.js
@@ -1,5 +1,14 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  Username: DS.attr('string'),
+
+  availableDate: DS.attr('string'),
+  username: DS.attr('string'),
+  createdBy: DS.attr('string'),
+  displayName: DS.attr('string'),
+  editedBy: DS.attr('string'),
+  isAvailable: DS.attr('boolean'),
+  jobCount: DS.attr('number'),
+  isActive: DS.attr('boolean')
+
 });
