1783ba6b07ca5d18d2adf9216e06a1258ca7d964
jkc07100 <keith.clinard@kub.org>
2017-03-13 14:35:54 -0400

    feat: Added basic firebase data integration

--- app/protected/route.js
+++ app/protected/route.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model(){
+    return Ember.RSVP.hash({
+      jobs: this.get('store').query('job', {
+        orderBy: "assignedTo",
+        equalTo: "maudevolk",
+      }),
+      workers: this.get('store').query('worker', {
+        orderBy: "isAvailable",
+        equalTo: true,
+      }),
+    });
+  },
 });
