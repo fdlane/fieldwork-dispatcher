46b81b0fcd45e56a60d295f7f58e81c46159a8d8
AmberRivera <amberolive@gmail.com>
2017-03-14 14:52:17 -0400

    Workers and jobs tables with realtime data.

---
+++ tests/integration/components/jobs-table/component-test.js
@@ -0,0 +1,12 @@
+import { moduleForComponent, test } from 'ember-qunit';
+//import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('jobs-table', 'Integration | Component | jobs table', {
+  integration: true
+});
+
+test('pass', function(assert) {
+
+  assert.ok(true, "implement when functionality added.");
+
+});
