8695213f8d5a094dddc96205ae5d77a7ff3c57f8
AmberRivera <amberolive@gmail.com>
2017-03-27 20:39:07 -0400

    Error handling for login

--- app/login/route.js
+++ app/login/route.js
@@ -9,29 +9,38 @@ export default Ember.Route.extend({
   },
 
   actions: {
-
     login(email, password) {
       let session = this.get('session');
       session.open('firebase', {
         provider: 'password',
         email,
         password
-
       }).then(() => {
-
         let user = session.content.currentUser;
         let email = user.email;
         let username = email.substring(0, email.indexOf('@'));
         user.username = username;
         this.set('currentUser.content', user);
         this.transitionTo('protected');
+      },
+    (error) => {
+      if(error) {
+        this.controller.set('invalidCredentials', true);
+      }
 
-      });
+    });
+    },
+
+    willTransition(transition) {
+      if(transition) {
+        this.controller.set('invalidCredentials', false);
+      }
     },
 
     forgotPassword() {
-      this.transitionTo('password-reset');
-    }
-  }
 
+      this.transitionTo('password-reset');
+
+    },
+  },
 });
