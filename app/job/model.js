173f3162825b6915a6e60705818c7bbad360facb
Donald E Nash <donaldnash1989@gmail.com>
2017-03-30 13:59:41 -0400

    updated job model (#13)

--- app/job/model.js
+++ app/job/model.js
@@ -1,13 +1,18 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
-  assignedTo: DS.attr('string'),
-  createdBy: DS.attr('string'),
-  editedBy: DS.attr('string'),
-  editedDate: DS.attr('string'),
-  location: DS.attr('string'),
-  metaId: DS.attr('string'),
-  isActive: DS.attr('boolean')
-
+    assignedTo: DS.attr('string'),
+    createdBy: DS.attr('string'),
+    editedBy: DS.attr('string'),
+    editedDate: DS.attr('string'),
+    location: DS.attr('string'),
+    metaId: DS.attr('string'),
+    isActive: DS.attr('boolean'),
+    description: DS.attr('string'),
+    createdDate: DS.attr('string'),
+    contactInfo: DS.attr('string'),
+    category: DS.attr('string'),
+    scheduleDate: DS.attr('string'),
+    status: DS.attr('string'),
+    jobImage: DS.attr('string'),
 });
