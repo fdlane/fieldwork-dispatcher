f7a6b6fdcea25a0b8e9a108e988b9872d7815a83
AmberRivera <amberolive@gmail.com>
2017-04-10 10:48:40 -0400

    Use Ember services

--- app/components/jobs-table/component.js
+++ app/components/jobs-table/component.js
@@ -2,19 +2,49 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  disableAssign: true,
+  jobService: Ember.inject.service('job'),
+
+  columns: [
+    {
+      "propertyName": "assignedTo",
+      "title": "Assigned To",
+      "sortDirection": "asc",
+      "sortPrecedence": 1,
+    },
+
+    {
+      "propertyName": "location",
+      "title": "Location",
+      "disableSorting": true,
+    },
+
+    {
+      "propertyName": "status",
+      "title": "Status"
+    }
+  ],
+
+  tableClasses: Ember.Object.create({
+    "table": "table  table-bordered",
+
+  }),
+
 
   actions: {
+    selectJob(table) {
+      this.get('jobService').selectJob(table);
+    },
+
     assign() {
-      this.sendAction('assign');
+      this.get('jobService').assignJob();
     },
 
     unassign() {
-      this.sendAction('unassign');
+      this.get('jobService').unassignJob();
     },
 
     cancel() {
-      this.sendAction('cancel');
+      this.get('jobService').cancelJob();
     }
   }
 });
